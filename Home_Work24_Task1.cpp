c6b6c61ca18b6f4b68ffdb2f5e7e4afd63ab4dc7
Pasteras <128259268+Pasteras@users.noreply.github.com>
2023-05-12 17:29:18 +0300

    Add files via upload

---
+++ Home_Work24_Task1.cpp
@@ -0,0 +1,32 @@
+﻿/*﻿Task 1. Given a letter string that contains the sequence of 
+characters s0, ..., sn, ... 
+Calculate the length of the longest word in this sequence. 
+A word is a group of symbols that are arranged consecutiveand 
+distinct from spacesand punctuation marks.*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void Str1(string &str);
+
+int main()
+{
+	string str = ".hello World";
+	cout << str << endl;
+	Str1(str);
+}
+
+void Str1(string &str)
+{
+	for (int i = 0; i < sizeof(str); i++)
+	{
+		if (str[i] = char(46))
+		{
+			str[i + 1] = toupper(str[i + 1]);
+			break;
+		}
+	}
+	cout << endl << str << endl;
+}

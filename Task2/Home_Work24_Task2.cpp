cda8cf54c3de87d40fe9c06bf495412d5ac2b73d
Pasteras <128259268+Pasteras@users.noreply.github.com>
2023-05-12 18:14:02 +0300

    Add files via upload

---
+++ Task2/Home_Work24_Task2.cpp
@@ -0,0 +1,47 @@
+﻿/*Task 2.Given a letter string that
+contains the sequence of characters s0, ...,sn, ...
+If the word is written after a period, then replace the first
+letter in this word with the appropriate uppercase letter.*/
+
+#include <iostream>
+#include <string.h>
+#include <cstring>
+#include <string>
+
+using namespace std;
+
+void Str(string* slova, string &max);
+void ShowStr(string* slova);
+
+int main()
+{
+    int len;
+    string slova[]{"Car", "Table", "T-short", "Dollar"};
+    string max = slova[0];
+    ShowStr(slova);
+    Str(slova, max);
+}
+
+void Str(string* slova, string &max)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if (slova[i].length() > max.length())
+            {
+                max = slova[i];
+            }
+        }
+    }
+    cout << "The longest word: " << max << endl;
+}
+
+void ShowStr(string* slova)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        cout << slova[i] << endl;
+    }
+    cout << endl << endl;
+}
